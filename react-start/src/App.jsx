fab483e2f6a3d110d506711d32d8f1835ee76f23
Vitos1992 <125671703+Vitos1992@users.noreply.github.com>
2026-02-15 23:10:34 +0200

    newd

--- react-start/src/App.jsx
+++ react-start/src/App.jsx
@@ -1,40 +1,28 @@
-import { useState } from "react";
+// import { useState } from "react";
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  const step = 5;
-
-  function increment() {
-    setCount(prev => prev + step);
-  }
-
-  function decrement() {
-    setCount(prev => Math.max(prev - step, 0));
-  }
-
-  function double() {
-    setCount(prev => prev * 2);
-  }
-
-  function reset() {
-    setCount(0);
-  }
-
+function Header() {
   return (
-    <div>
-      <h1>Мій перший проєки на React</h1>
-      <h2>Лічильник: {count}</h2>
+    <header>
+        <h2>логотип</h2>
 
-      <button onClick={increment}>+{step}</button>
-      <button onClick={double}>*2</button>
-      <button onClick={decrement}>-{step}</button>
-      <button onClick={reset}>Reset</button>
-    </div>  
+        <p>опис</p>
+    </header>
   );
 }
 
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h1>Hello Vitala</h1>
+
+        <p>Вітаю з першим проєктом на React </p>
+      </main>
+    </div>
+  );
+}
 
 export default App
 
