95d84671c26d44e50e03aa271702b8596780a500
Vitos1992 <125671703+Vitos1992@users.noreply.github.com>
2026-02-27 21:11:48 +0200

    react

--- react-start/src/components/Feedback.jsx
+++ react-start/src/components/Feedback.jsx
@@ -1,6 +1,15 @@
 import Button from "./Button/Button";
+import { useState } from "react";
 
 export default function Feedback() {
+
+    const [name, setName] =  useState('')
+    const [services, setServices] =  useState('teamlid')
+
+    function handLeNameCange(event) {
+        setName(event.target.value)
+    }
+    
     return (
         <section className="feedback">
             <div>
@@ -9,14 +18,22 @@ export default function Feedback() {
                 <from className="form_box">
                     <div className="services_from">
                         <label htmlFor="name" className="form_label">First Name</label>
-                        <input type="text" id="name" className="control"/>
+                        <input type="text" id="name" className="control" value={name} onChange={handLeNameCange} />
+
                         <label htmlFor="services" className="form_label">Services</label>
-                        <select id="services" className="control">
+                        <select id="services" className="control" value={services} onChange={event => setServices(event.target.value)} >
                             <option className="control_option" value="teamlid">Team lid</option>
                             <option className="control_option" value="front-end">Front-end develepor</option>
                             <option className="control_option" value="back-end">Back-end develepor</option>
                         </select>
                     </div>
+
+                    {/* <pre>
+                        Name: {name}
+                        <br />
+                        Services: {services}
+                    </pre> */}
+
                     <Button>SEND</Button>
                 </from>
             </div>
